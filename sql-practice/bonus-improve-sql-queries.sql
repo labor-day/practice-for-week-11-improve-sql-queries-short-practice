e009c5aa93bff2be62ea862f326e805259efd485
labor-day <shaffer.scott@yahoo.com>
2022-08-16 23:35:29 -0600

    efefficient queries

--- sql-practice/bonus-improve-sql-queries.sql
+++ sql-practice/bonus-improve-sql-queries.sql
@@ -1,13 +1,19 @@
 ----------
--- Step 0 - Create a Query 
+-- Step 0 - Create a Query
 ----------
 -- Query: Find a count of `toys` records that have a price greater than
     -- 55 and belong to a cat that has the color "Olive".
 
     -- Your code here
+    EXPLAIN QUERY PLAN
+    SELECT COUNT(*)
+    FROM toys JOIN cat_toys
+    ON toys.id = cat_toys.toy_id
+    JOIN cats ON cats.id = cat_toys.cat_id
+    WHERE toys.price > 55 AND cats.color = 'Olive';
 
 -- Paste your results below (as a comment):
-
+-- 215
 
 
 
@@ -19,15 +25,19 @@
     -- Your code here
 
 -- Paste your results below (as a comment):
-
+/*
+|--SCAN TABLE cat_toys
+|--SEARCH TABLE toys USING INTEGER PRIMARY KEY (rowid=?)
+ --SEARCH TABLE cats USING INTEGER PRIMARY KEY (rowid=?)
+*/
 
 -- What do your results mean?
 
     -- Was this a SEARCH or SCAN?
-
+    -- We conduct 2 searches and a scan
 
     -- What does that mean?
-
+    -- efficiency can be improved by creating indexes
 
 
 
@@ -39,8 +49,7 @@
     -- Your code here
 
 -- Paste your results below (as a comment):
-
-
+-- Run Time: real 0.004 user 0.003746 sys 0.000749
 
 
 ----------
@@ -50,18 +59,30 @@
 -- Create index:
 
     -- Your code here
+    CREATE INDEX idx_cat_toys_cat_id_toy_id
+    ON cat_toys(toy_id, cat_id);
 
 -- Analyze Query:
     -- Your code here
+    EXPLAIN QUERY PLAN
+    SELECT COUNT(*)
+    FROM toys JOIN cat_toys
+    ON toys.id = cat_toys.toy_id
+    JOIN cats ON cats.id = cat_toys.cat_id
+    WHERE toys.price > 55 AND cats.color = 'Olive';
 
 -- Paste your results below (as a comment):
+--QUERY PLAN
+--SCAN TABLE cat_toys
+--SEARCH TABLE toys USING INTEGER PRIMARY KEY (rowid=?)
+--SEARCH TABLE cats USING INTEGER PRIMARY KEY (rowid=?)
+--Run Time: real 0.004 user 0.000000 sys 0.004121
 
 
 -- Analyze Results:
 
     -- Is the new index being applied in this query?
-
-
+    -- The new index is not used
 
 
 ----------
@@ -88,3 +109,31 @@
 ---------------------------------
 -- Notes From Further Exploration
 ---------------------------------
+
+-- creates indexes for price and name
+CREATE INDEX toys_price ON toys(price);
+CREATE INDEX cats_name ON cats(name);
+
+--subqueries for use in outer query
+SELECT *
+FROM toys
+WHERE toys.price > 55
+
+SELECT *
+FROM cats
+WHERE cats.name = 'Olive'
+
+EXPLAIN QUERY PLAN
+SELECT COUNT(cat_toys.id)
+FROM cat_toys
+WHERE cat_toys.toy_id IN (SELECT toys.id FROM toys WHERE toys.price > 55)
+    AND cat_toys.cat_id IN (SELECT cats.id FROM cats WHERE cats.name = 'Olive');
+
+--QUERY PLAN
+--SEARCH TABLE cat_toys USING COVERING INDEX idx_cat_toys_cat_id_toy_id (toy_id=? AND cat_id=?)
+--LIST SUBQUERY 1
+    --SEARCH TABLE toys USING COVERING INDEX toys_price (price>?)
+--LIST SUBQUERY 2
+    --SEARCH TABLE cats USING COVERING INDEX cats_name (name=?)
+-- Run Time: real 0.003 user 0.001997 sys 0.000544
+-- now search through all tables, significantly increasing efficiency
--- sql-practice/improve-sql-queries.sql
+++ sql-practice/improve-sql-queries.sql
@@ -1,12 +1,22 @@
 ----------
--- Step 0 - Create a Query 
+-- Step 0 - Create a Query
 ----------
 -- Query: Select all cats that have a toy with an id of 5
 
     -- Your code here
+    /*
+    SELECT cats.name
+    FROM cats JOIN cat_toys ON cats.id = cat_toys.cat_id
+    WHERE cat_toys.toy_id = 5;
+    */
 
 -- Paste your results below (as a comment):
 
+    /*
+    Rachele
+    Rodger
+    Jamal
+    */
 
 
 
@@ -16,19 +26,32 @@
 -- Query:
 
     -- Your code here
+    EXPLAIN QUERY PLAN
+        SELECT cats.name
+    FROM cats JOIN cat_toys ON cats.id = cat_toys.cat_id
+    WHERE cat_toys.toy_id = 5;
 
 -- Paste your results below (as a comment):
+
+/*
+SCAN TABLE cat_toys
+SEARCH TABLE cats USING INTEGER PRIMARY KEY (rowid=?)
+*/
 
 
 -- What do your results mean?
 
     -- Was this a SEARCH or SCAN?
-
+    /* The cats table was searched
+    because the primary key is part of an index.
+    The cat_toys table was scanned because we needed to look
+    through every toy_id
+    */
 
     -- What does that mean?
-
-
-
+    /* This operation can be improved
+    if we can search through the cat_toys table as well
+    */
 
 ----------
 -- Step 2 - Time the Query to get a baseline
@@ -38,8 +61,7 @@
     -- Your code here
 
 -- Paste your results below (as a comment):
-
-
+-- Run Time: real 0.001 user 0.000064 sys 0.000016
 
 
 ----------
@@ -49,6 +71,14 @@
 -- Create index:
 
     -- Your code here
+    EXPLAIN QUERY PLAN
+    SELECT cats.name
+        FROM cats
+        JOIN cat_toys
+        ON cats.id = cat_toys.cat_id
+        JOIN toys
+        ON toys.id = cat_toys.toy_id
+        WHERE cat_toys.toy_id = 5;
 
 -- Analyze Query:
     -- Your code here
@@ -61,8 +91,6 @@
     -- Is the new index being applied in this query?
 
 
-
-
 ----------
 -- Step 4 - Re-time the query using the new index
 ----------
@@ -72,13 +100,20 @@
 
 -- Paste your results below (as a comment):
 
+--SEARCH TABLE toys USING INTEGER PRIMARY KEY (rowid=?)
+--SCAN TABLE cat_toys
+--SEARCH TABLE cats USING INTEGER PRIMARY KEY (rowid=?)
+--Run Time: real 0.000 user 0.000062 sys 0.000015
+
 
 -- Analyze Results:
     -- Are you still getting the correct query results?
-
+    -- the result are the same
 
     -- Did the execution time improve (decrease)?
-
+    /* the execution time barely improved
+    We still scan through the cat_toys table
+    and the time taken to search the other two tables is neglible
 
     -- Do you see any other opportunities for making this query more efficient?
 
@@ -86,3 +121,14 @@
 ---------------------------------
 -- Notes From Further Exploration
 ---------------------------------
+CREATE INDEX idx_cat_toys_toy_id
+ON cat_toys(toy_id);
+
+--SEARCH TABLE cat_toys USING INDEX idx_cat_toys_toy_id (toy_id=?)
+--SEARCH TABLE cats USING INTEGER PRIMARY KEY (rowid=?)
+Run Time: real 0.000 user 0.000075 sys 0.000000
+
+By creating an index for the toy_id column in the cat_toys
+table, we now execute 2 searches and 0 scans
+This has significantly increased efficiency
+*/
